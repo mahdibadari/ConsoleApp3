24753a73d873e7456c95f8792f621fd167fac672
mahdibadari-mitrais <mahdi.badari@mitrais.com>
2024-05-07 15:07:56 +0700

    fixed the program cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,51 +2,25 @@
 using ConsoleApp3.Function;
 using ConsoleApp3.Interfaces;
 
-DateTime testInput = new DateTime(2025, 5, 7);
-DateFunction dateFunction = new DateFunction();
-dateFunction.GetNextMonthDateCount(testInput);
-
-int[] arrayInput = [1, 2, 3, 4, 5, 6, 7, 8];
-ArrayFunction arrayFunction = new ArrayFunction();
-arrayFunction.AddOnlyOdd(arrayInput);
-
-string[] nameInput = ["Sandro", "Indra"];
-string[] secondNameInput = ["Mawar", "Melati", "Indra"];
-NameFunction nameFunction = new NameFunction();
-nameFunction.UniqueNames(nameInput, secondNameInput);
-
-
 namespace ConsoleApp3
 {
-    //internal class Main
-    //{
-    //        private IDateFunction _dateFunction;
-    //        private IArrayFunction _arrayFunction;
-    //        private INameFunction _nameFunction;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime testInput = new DateTime(2025, 5, 7);
+            IDateFunction dateFunction = new DateFunction();
+            dateFunction.GetNextMonthDateCount(testInput);
 
-    //        internal Main()
-    //        {
-    //            _arrayFunction = new ArrayFunction();
-    //            _dateFunction = new DateFunction();
-    //            _nameFunction = new NameFunction();
+            int[] arrayInput = [1, 2, 3, 4, 5, 6, 7, 8];
+            IArrayFunction arrayFunction = new ArrayFunction();
+            arrayFunction.AddOnlyOdd(arrayInput);
 
-    //            _arrayFunction.AddOnlyOdd([1, 2, 3, 4, 5, 6, 7, 8]);
-    //            _nameFunction.UniqueNames(["Sandro", "Indra"], ["Mawar", "Melati", "Indra"]);
-    //            _dateFunction.GetNextMonthDateCount(new DateTime(2025, 5, 7));
-    //        }
-
-    //        DateTime testInput = new DateTime(2025, 5, 7);
-    //        DateFunction dateFunction = new DateFunction();
-    //        dateFunction.GetNextMonthDateCount(testInput);
-
-    //int[] arrayInput = [1, 2, 3, 4, 5, 6, 7, 8];
-    //        ArrayFunction arrayFunction = new ArrayFunction();
-    //        arrayFunction.AddOnlyOdd(arrayInput);
-
-    //string[] nameInput = ["Sandro", "Indra"];
-    //        string[] secondNameInput = ["Mawar", "Melati", "Indra"];
-    //        NameFunction nameFunction = new NameFunction();
-    //        nameFunction.UniqueNames(nameInput, secondNameInput);
-    //}
+            string[] nameInput = ["Sandro", "Indra"];
+            string[] secondNameInput = ["Mawar", "Melati", "Indra"];
+            INameFunction nameFunction = new NameFunction();
+            nameFunction.UniqueNames(nameInput, secondNameInput);
+        }
+    }
 }
 
